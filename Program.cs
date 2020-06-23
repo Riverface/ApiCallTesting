f1244878adea6347d00f6d58381339c6b49a6558
Riverface <sonoskeakigawa@gmail.com>
2020-06-23 10:18:28 -0700

    DeSerialization added

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+namespace TopStories
+{
+    public class Article
+    {
+        public string Section { get; set; }
+        public string Title { get; set; }
+        public string Abstract { get; set; }
+        public string Url { get; set; }
+        public string Byline { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var apiCallTask = ApiHelper.ApiCall("BXjGG7wWhdATCF5wGBWVlAN2elpL6ifp");
+            var result = apiCallTask.Result;
+            JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
+            List<Article> articleList = JsonConvert.DeserializeObject<List<Article>>(jsonResponse["results"].ToString());
+            foreach (Article article in articleList)
+            {
+                Console.WriteLine($"Section: {article.Section}");
+                Console.WriteLine($"Title: {article.Title}");
+                Console.WriteLine($"Abstract: {article.Abstract}");
+                Console.WriteLine($"Url: {article.Url}");
+                Console.WriteLine($"Byline: {article.Byline}");
+                Console.WriteLine("====================");
+            }
+        }
+    }
+
+    class ApiHelper
+    {
+        public static async Task<string> ApiCall(string apiKey)
+        {
+            RestClient client = new RestClient("https://api.nytimes.com/svc/topstories/v2");
+            RestRequest request = new RestRequest($"home.json?api-key={apiKey}", Method.GET);
+            var response = await client.ExecuteTaskAsync(request);
+            return response.Content;
+        }
+    }
+}
